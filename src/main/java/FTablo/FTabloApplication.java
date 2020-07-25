36d8a3957d521715827315d784a2e3d186d84b87
Anatoliy Berelekhis <katolomobil@gmail.com>
2020-07-25 23:55:18 +0300

    Initial commit

---
+++ src/main/java/FTablo/FTabloApplication.java
@@ -0,0 +1,13 @@
+package FTablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FTabloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FTabloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FTablo/FTabloApplicationTests.java
@@ -0,0 +1,13 @@
+package FTablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FTabloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
