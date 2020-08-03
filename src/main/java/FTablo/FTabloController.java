8817900471ad47a48a9bf4134ca0d4c50b45bbe3
katol <katolomobil@gmail.com>
2020-08-03 22:01:45 +0300

    Opportunity to start fanse ftablo vue Thymeleaf

--- src/main/java/FTablo/FTabloController.java
+++ src/main/java/FTablo/FTabloController.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SecretaryController {
+public class FTabloController {
     @GetMapping("/secretary")
     public String secretary() {
         return "secretary";
     }
 
-    public String fance() {
-        return "fance";
+    @GetMapping("/fanse")
+    public String fanse() {
+        return "fanse";
     }
 }
