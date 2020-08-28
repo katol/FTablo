9080b5e6a9243d0aa0a7a6ccff4cabde9b535a0c
katol <katolomobil@gmail.com>
2020-08-28 23:34:36 +0300

    Adding lombok and using it in Fight entity

--- src/main/java/FTablo/entities/Fight.java
+++ src/main/java/FTablo/entities/Fight.java
@@ -1,11 +1,13 @@
 package FTablo.entities;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
-import java.util.Objects;
 
 @Entity
 @Table(name = "fights")
+@Data
 public class Fight {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,94 +33,4 @@ public class Fight {
 
     @Column
     private Integer blueScores;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Fight fight = (Fight) o;
-        return id.equals(fight.id) &&
-                secondsPassed.equals(fight.secondsPassed) &&
-                endTs.equals(fight.endTs) &&
-                endDescription.equals(fight.endDescription) &&
-                redName.equals(fight.redName) &&
-                redScores.equals(fight.redScores) &&
-                blueName.equals(fight.blueName) &&
-                blueScores.equals(fight.blueScores);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, secondsPassed, endTs, endDescription, redName, redScores, blueName, blueScores);
-    }
-
-    @Override
-    public String toString() {
-        return "Fight{" +
-                "id=" + id +
-                ", secondsPassed=" + secondsPassed +
-                ", endTs=" + endTs +
-                ", endDescription='" + endDescription + '\'' +
-                ", redName='" + redName + '\'' +
-                ", redScores=" + redScores +
-                ", blueName='" + blueName + '\'' +
-                ", blueScores=" + blueScores +
-                '}';
-    }
-
-    public Integer getSecondsPassed() {
-        return secondsPassed;
-    }
-
-    public void setSecondsPassed(Integer secondsPassed) {
-        this.secondsPassed = secondsPassed;
-    }
-
-    public Timestamp getEndTs() {
-        return endTs;
-    }
-
-    public void setEndTs(Timestamp endTs) {
-        this.endTs = endTs;
-    }
-
-    public String getEndDescription() {
-        return endDescription;
-    }
-
-    public void setEndDescription(String endDescription) {
-        this.endDescription = endDescription;
-    }
-
-    public String getRedName() {
-        return redName;
-    }
-
-    public void setRedName(String redName) {
-        this.redName = redName;
-    }
-
-    public Integer getRedScores() {
-        return redScores;
-    }
-
-    public void setRedScores(Integer redScores) {
-        this.redScores = redScores;
-    }
-
-    public String getBlueName() {
-        return blueName;
-    }
-
-    public void setBlueName(String blueName) {
-        this.blueName = blueName;
-    }
-
-    public Integer getBlueScores() {
-        return blueScores;
-    }
-
-    public void setBlueScores(Integer blueScores) {
-        this.blueScores = blueScores;
-    }
 }
