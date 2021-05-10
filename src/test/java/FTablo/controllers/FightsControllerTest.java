1d45af511ea5b0aaa96298096a7558c1f6b5aa54
aberelekhis@gmail.com <aberelekhis@gmail.com>
2021-05-10 15:30:25 +0300

    Spring REST API docs

--- src/test/java/FTablo/controllers/FightsControllerTest.java
+++ src/test/java/FTablo/controllers/FightsControllerTest.java
@@ -3,17 +3,20 @@ package FTablo.controllers;
 import FTablo.services.FightsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FightsController.class)
+@AutoConfigureRestDocs(outputDir = "build/snippets")
 class FightsControllerTest {
 
     @Autowired
@@ -26,37 +29,41 @@ class FightsControllerTest {
     void createFight_returnsStatus201() throws Exception {
         String json = "{ \"redName\":\"Sveta\", \"blueName\":\"Tolian\" }";
         mockMvc.perform(post("/fights").contentType(MediaType.APPLICATION_JSON).content(json))
-                .andDo(print()).andExpect(status().isCreated());
+                .andDo(print()).andExpect(status().isCreated()).andDo(document("fights/create"));
     }
 
     @Test
     void createFight_throwsException_WhenInvalidParameters() throws Exception {
         String json = "{  }";
         mockMvc.perform(post("/fights").contentType(MediaType.APPLICATION_JSON).content(json))
-                .andDo(print()).andExpect(status().isBadRequest());
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andDo(document("fights/create-with-invalid-parameters"));
     }
 
     @Test
     void getFights_returnsStatus200() throws Exception {
-        mockMvc.perform(get("/fights")).andDo(print()).andExpect(status().isOk());
+        mockMvc.perform(get("/fights")).andDo(print()).andExpect(status().isOk())
+                .andDo(document("fights/get-all"));
     }
 
     @Test
     void getFight_returnsStatus200() throws Exception {
-        mockMvc.perform(get("/fights/1")).andDo(print()).andExpect(status().isOk());
+        mockMvc.perform(get("/fights/1")).andDo(print()).andExpect(status().isOk())
+                .andDo(document("fights/get-by-id"));
     }
 
     @Test
     void addExchangeToFight() throws Exception {
         String json = "{ \"secondsPassed\":25, \"saveTs\":\"2020-09-23T08:12:56.7747\" }";
         mockMvc.perform(post("/fights/1/exchanges").contentType(MediaType.APPLICATION_JSON).content(json))
-                .andDo(print()).andExpect(status().isCreated());
+                .andDo(print()).andExpect(status().isCreated()).andDo(document("fights/add-exchange"));
     }
 
     @Test
     void addExchangeToFight_throwsException_WhenInvalidParameters() throws Exception {
         String json = "{  }";
         mockMvc.perform(post("/fights/1/exchanges").contentType(MediaType.APPLICATION_JSON).content(json))
-                .andDo(print()).andExpect(status().isBadRequest());
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andDo(document("fights/add-exchange-with-invalid-parameters"));
     }
 }
